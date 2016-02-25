49a14f84d365e0c5fec7a8446a7963baf86a6eef
Tomas Petricek <tomas@tomasp.net>
2016-02-25 16:17:33 +0000

    Paket update, use FSharp.Data

--- app.fsx
+++ app.fsx
@@ -1,4 +1,5 @@
 ﻿#r "packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
+#r "packages/FSharp.Data/lib/net40/FSharp.Data.dll"
 #r "packages/Suave/lib/net40/Suave.dll"
 #r "System.Runtime.Serialization.dll"
 #load "paket-files/tpetricek/fsharp-web-editors/server/editor.fs"
